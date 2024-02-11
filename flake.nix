04e0f23f0fbaf67ec5d6b833800572b5ff3bfdab
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-11 18:39:57 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "faniryxx's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      # please change the hostname to your own
+      nixos-vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # replace ryan with your own username
+            home-manager.users.faniryxx = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,96 @@
+{ config, pkgs, ... }:
+
+{
+  # please change the username & home direcotry to your own
+  home.username = "faniryxx";
+  home.homeDirectory = "/home/faniryxx";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # set cursor size and dpi for 4k monitor
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    zsh
+    neofetch
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    dnsutils  # `dig` + `nslookup`
+    
+    cowsay
+    # file
+    which
+    gnupg
+
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
+
+    # productivity
+    # hugo # static site generator
+    # glow # markdown previewer in terminal
+
+    btop  # replacement of htop/nmon
+    # iotop # io monitoring
+    # iftop # network monitoring
+
+    # system call monitoring
+    # strace # system call monitoring
+    # ltrace # library call monitoring
+    # lsof # list open files
+
+    # system tools
+    # sysstat
+    lm_sensors # for `sensors` command
+    # ethtool
+    pciutils # lspci
+    usbutils # lsusb
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "Nathan RANAIVO RAVOAJA";
+    userEmail = "faniry597@gmail.com";
+  };
+
+  # This value determines the home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update home Manager without changing this value. See
+  # the home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.11";
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
