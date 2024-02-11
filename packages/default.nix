77b40b96834fd92b45712596ea899ef6ffaf6779
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-11 21:11:30 +0100

    refactor: add packages to packages/default.nix

--- packages/default.nix
+++ packages/default.nix
@@ -11,6 +11,8 @@
         gnomeExtensions.blur-my-shell
         gnomeExtensions.dash-to-dock
         gnomeExtensions.pop-shell
+        gnomeExtensions.color-picker
+        terminator
         vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         vscode
         wget
