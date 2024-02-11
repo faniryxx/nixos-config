e2c15753b52fd385915270d5484841314c4b6d72
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-11 19:58:28 +0100

    fix: correct path for packages/default.nix

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -8,7 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ../packages/default.nix
+      ../../packages/default.nix
     ];
 
   # Bootloader.
