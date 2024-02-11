d93d54ac7b134e9f535d8799ada24c64b1ade8ec
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-11 19:47:47 +0100

    feat: add some gnome extensions to nixos-vm

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -96,13 +96,16 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  git
-  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  wget
+  environment.systemPackages = with pkgs; [ 
   curl
-  
+  git
+  gnome.gnome-tweaks
+  gnomeExtensions.blur-my-shell
+  gnomeExtensions.dash-to-dock
+  gnomeExtensions.pop-shell
+  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   vscode
+  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
