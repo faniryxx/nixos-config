cb5e4c1f9926acac98e4317647ad9f1cba6fea19
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-12 22:44:51 +0100

    fix: disable gnome app shortcuts

--- gnome/default.nix
+++ gnome/default.nix
@@ -55,6 +55,18 @@
                     "pop-shell@system76.com"
                 ];
             };
+            # Disable Gnome Super+number app shortcuts
+            "org/gnome/shell/keybindings" = {
+                switch-to-application-1 = [];
+                switch-to-application-2 = [];
+                switch-to-application-3 = [];
+                switch-to-application-4 = [];
+                switch-to-application-5 = [];
+                switch-to-application-6 = [];
+                switch-to-application-7 = [];
+                switch-to-application-8 = [];
+                switch-to-application-9 = [];
+            };
         };
     };
 }
