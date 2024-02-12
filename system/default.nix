c71ac0e6e0bc49bb98aa1a2c3d8e3592caecc3d0
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-12 23:34:50 +0100

    fix: remove FR locale settings

--- system/default.nix
+++ system/default.nix
@@ -15,18 +15,6 @@
     # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
 
-    i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr_FR.UTF-8";
-    LC_IDENTIFICATION = "fr_FR.UTF-8";
-    LC_MEASUREMENT = "fr_FR.UTF-8";
-    LC_MONETARY = "fr_FR.UTF-8";
-    LC_NAME = "fr_FR.UTF-8";
-    LC_NUMERIC = "fr_FR.UTF-8";
-    LC_PAPER = "fr_FR.UTF-8";
-    LC_TELEPHONE = "fr_FR.UTF-8";
-    LC_TIME = "fr_FR.UTF-8";
-    };
-
     # Enable flakes
     nix.settings.experimental-features = ["nix-command" "flakes"];
 
@@ -42,8 +30,8 @@
 
     # Configure keymap in X11
     services.xserver = {
-    xkb.layout = "fr";
-    xkb.variant = "";
+        xkb.layout = "fr";
+        xkb.variant = "";
     };
 
     # Configure console keymap
