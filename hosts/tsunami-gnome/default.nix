fc6a95dd170fb7cc9346fd6cf7598b930cd90ae1
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-14 17:27:20 +0100

    refactor: move gdm and gnome activation statements to hosts file instead of system/default.nix

--- hosts/tsunami-gnome/default.nix
+++ hosts/tsunami-gnome/default.nix
@@ -16,6 +16,10 @@
 
   networking.hostName = "tsunami"; # Define your hostname.
 
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
   environment.systemPackages = with pkgs; [
     openrgb-with-all-plugins
     i2c-tools
