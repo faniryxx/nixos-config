38735e89de77b14ee9fdc1fd365c7904f2ae937e
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-15 16:48:41 +0100

    feat: configure wlogout to work with swaylock

--- deployment/deploy_tsunami_hyprland.sh
+++ deployment/deploy_tsunami_hyprland.sh
@@ -2,9 +2,17 @@
 # Rebuild system
 nixos-rebuild switch --flake /home/faniryxx/nixos-config#tsunami-hyprland
 
-echo "Copying hyprland configuration to correct folder..."
+echo "Copying hyprland configuration to ~/.config ..."
+mkdir -p /home/faniryxx/.config/hypr/
 cp /home/faniryxx/nixos-config/.config/hypr/tsunami.conf /home/faniryxx/.config/hypr/hyprland.conf
 
-echo "Copying waybar configuration to correct folder..."
+echo "Copying waybar configuration to ~/.config ..."
+mkdir -p /home/faniryxx/.config/waybar/
 cp /home/faniryxx/nixos-config/.config/waybar/config /home/faniryxx/.config/waybar/config
 cp /home/faniryxx/nixos-config/.config/waybar/style.css /home/faniryxx/.config/waybar/style.css
+
+echo "Copying wlogout configuration to ~/.config ..."
+mkdir -p /home/faniryxx/.config/wlogout/
+cp /home/faniryxx/nixos-config/.config/wlogout/layout /home/faniryxx/.config/wlogout/layout
+cp /home/faniryxx/nixos-config/.config/wlogout/style.css /home/faniryxx/.config/wlogout/style.css
+cp -r /home/faniryxx/nixos-config/.config/wlogout/icons /home/faniryxx/.config/wlogout/icons
