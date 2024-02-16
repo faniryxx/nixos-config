6a02fd77f5ffa0764d357f065bea6089488884aa
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-16 13:55:03 +0100

    feat: transpose tsunami config to lagoon host

---
+++ home-manager/lagoon-hyprland.nix
@@ -0,0 +1,16 @@
+# hyprland specific options for host tsunami
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./home.nix
+    ./hyprland.nix
+    ];
+
+  home.file = {
+    # hyprland config
+    ".config/hypr/hyprland.conf" = {
+      source = config.lib.file.mkOutOfStoreSymlink ../.config/hypr/lagoon/hyprland.conf;
+    };
+  };
+}
