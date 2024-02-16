31ca510ced63baf4ca42ed2285c5708c909120de
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-16 13:53:28 +0100

    refactor: factorize home manager configs further

--- home-manager/tsunami-hyprland.nix
+++ home-manager/tsunami-hyprland.nix
@@ -2,58 +2,15 @@
 { config, pkgs, ... }:
 
 {
-  imports = [./home.nix];
-  
-  # For future reference:
-  # To modify cursorTheme, declare it here
-  # Set it in nwg-look and ONLY export ~/.icons/default/index.theme and ~/.config/xsettingsd/xsettingsd.conf
-  gtk.enable = true;
-  gtk.theme.name = "Orchis-Teal-Dark";
-  gtk.theme.package = pkgs.orchis-theme;
-  gtk.cursorTheme.name = "Bibata-Original-Classic";
-  gtk.cursorTheme.package = pkgs.bibata-cursors;
+  imports = [
+    ./home.nix
+    ./hyprland.nix
+    ];
 
-  # Dotfiles management
   home.file = {
-    # Fix for dolphin white background on adwaita-dark theme
-    ".config/kdeglobals" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/kdeglobals;
-    };
     # hyprland config
     ".config/hypr/hyprland.conf" = {
       source = config.lib.file.mkOutOfStoreSymlink ../.config/hypr/tsunami/hyprland.conf;
-    };
-    # waybar config
-    ".config/waybar/config" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/waybar/config;
-    };
-    ".config/waybar/style.css" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/waybar/style.css;
-    };
-    # wlogout config
-    ".config/wlogout/layout" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/wlogout/layout;
-    };
-    ".config/wlogout/style.css" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/wlogout/style.css;
-    };
-    ".config/wlogout/icons/hibernate.png" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/wlogout/icons/hibernate.png;
-    };
-    ".config/wlogout/icons/lock.png" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/wlogout/icons/lock.png;
-    };
-    ".config/wlogout/icons/logout.png" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/wlogout/icons/logout.png;
-    };
-    ".config/wlogout/icons/reboot.png" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/wlogout/icons/reboot.png;
-    };
-    ".config/wlogout/icons/shutdown.png" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/wlogout/icons/shutdown.png;
-    };
-    ".config/wlogout/icons/suspend.png" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/wlogout/icons/suspend.png;
     };
   };
 }
