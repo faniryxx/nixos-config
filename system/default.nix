3cd71626a15d8ffcb18b4f79c7abc3ad022896ae
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-02-19 23:58:02 +0100

    fix: enable gvfs

--- system/default.nix
+++ system/default.nix
@@ -46,6 +46,9 @@
     # Enable CUPS to print documents.
     services.printing.enable = true;
 
+    # Enable virtual filesystems support
+    services.gvfs.enable = true;
+
     # Enable sound with pipewire.
     sound.enable = true;
     hardware.pulseaudio.enable = false;
