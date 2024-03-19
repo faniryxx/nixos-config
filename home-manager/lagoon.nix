ecfd7179c0d7c3a01b48732fdb8306a486c1afb9
Nathan RANAIVO RAVOAJA <faniry597@gmail.com>
2024-03-19 14:48:37 +0100

    refactor: manage hyprland configuration directly from home manager

--- home-manager/lagoon.nix
+++ home-manager/lagoon.nix
@@ -6,10 +6,46 @@
     ./home.nix
     ];
 
-  home.file = {
-    # hyprland config
-    ".config/hypr/hyprland.conf" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/hypr/lagoon/hyprland.conf;
-    };
-  };
+  # lagoon-specific hyprland configuration
+  wayland.windowManager.hyprland.extraConfig = ''
+    # See https://wiki.hyprland.org/Configuring/Monitors/
+    monitor=eDP-2, 1920x1200@60, 0x0, 1
+    workspace=1,monitor:eDP-2,defaut:true,persistent:true
+    workspace=2,monitor:eDP-2,persistent:true
+    workspace=3,monitor:eDP-2,persistent:true
+    workspace=4,monitor:eDP-2,persistent:true
+
+    decoration {
+    # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+    rounding = 10
+    
+    blur {
+        enabled = false
+        size = 3
+        passes = 1
+      }
+
+    drop_shadow = false
+    shadow_range = 4
+    shadow_render_power = 3
+    col.shadow = rgba(1a1a1aee)
+    }
+
+    bind = , XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_SOURCE@ toggle 
+
+    # Keyboard backlight
+    bind = , xf86KbdBrightnessUp, exec, brightnessctl -d *::kbd_backlight set +33%
+    bind = , xf86KbdBrightnessDown, exec, brightnessctl -d *::kbd_backlight set 33%-
+
+    # Screen brightness
+    bind = , XF86MonBrightnessUp, exec, brightnessctl s +5%
+    bind = , XF86MonBrightnessDown, exec, brightnessctl s 5%-
+
+    # rog-control-center
+    bind=,XF86Launch1,exec,rog-control-center
+
+    # asusctl profile
+    bind=,XF86Launch4,exec,asusctl profile -n
+  '';
 }
--- home-manager/tsunami.nix
+++ home-manager/tsunami.nix
@@ -6,10 +6,38 @@
     ./home.nix
     ];
 
-  home.file = {
-    # hyprland config
-    ".config/hypr/hyprland.conf" = {
-      source = config.lib.file.mkOutOfStoreSymlink ../.config/hypr/tsunami/hyprland.conf;
-    };
-  };
+  # tsunami-specific hyprland configuration
+  wayland.windowManager.hyprland.extraConfig = ''
+    exec-once = sleep 20 && openrgb --startminimized --profile "ChillOrange"
+    
+    # See https://wiki.hyprland.org/Configuring/Monitors/
+    monitor=DP-2, 3440x1440@144, 1080x200, 1
+    monitor=HDMI-A-1, 1920x1080@144, 0x0, 1, transform, 3
+    workspace=1,monitor:DP-2,defaut:true,persistent:true
+    workspace=2,monitor:DP-2,persistent:true
+    workspace=3,monitor:DP-2,persistent:true
+    workspace=4,monitor:DP-2,persistent:true
+    workspace=5,monitor:HDMI-A-1,persistent:true
+    exec-once=xrandr --output DP-2 --primary
+
+    decoration {
+    # See https://wiki.hyprland.org/Configuring/Variables/ for more
+
+    rounding = 10
+    
+    blur {
+        enabled = true
+        size = 3
+        passes = 1
+    }
+
+    drop_shadow = true
+    shadow_range = 4
+    shadow_render_power = 3
+    col.shadow = rgba(1a1a1aee)
+    }
+
+    # Waybar background blur
+    blurls = waybar
+  '';
 }
